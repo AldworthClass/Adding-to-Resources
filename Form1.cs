be598c9120405136901ca82cf85986ea45dcbb60
Steve Aldworth <s.aldworth@tvdsb.ca>
2026-02-03 10:22:19 -0500

    Challenges included

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,7 @@ namespace Adding_to_Resources
 {
     public partial class FormAddingResources : Form
     {
+        Random generator = new Random();
         public FormAddingResources()
         {
             InitializeComponent();
@@ -19,12 +20,29 @@ namespace Adding_to_Resources
 
         private void imgMario_Click(object sender, EventArgs e)
         {
-            imgMario.Image = Properties.Resources.MarioAwake;
+            int randNum;
+            randNum = generator.Next(5);    // generates a random number from 0-4
+            if (randNum == 0) // There is a 1 in 5 chance the number is a 0
+            {
+                imgMario.Image = Properties.Resources.Mario_Rage;
+            }
+            else
+                imgMario.Image = Properties.Resources.MarioAwake;
         }
 
         private void imgSleep_Click(object sender, EventArgs e)
         {
             imgMario.Image = Properties.Resources.MarioSleep;
+        }
+
+        private void imgMario_MouseEnter(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+
+        private void imgMario_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Default;
         }
     }
 }
